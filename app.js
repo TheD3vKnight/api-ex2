8cbd8696382dfd686d62169af6d858262ea6f15b
Aaron Lightfoot <lighttouch@Aarons-MBP.carolina.rr.com>
2018-02-20 09:24:14 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,38 @@
+let express = require('express')
+let assert = require('assert')
+let MongoClient = require('mongodb').MongoClient
+let devices = require('./devices.json')
+
+let app = express()
+
+let db
+
+MongoClient.connect('mongodb://TheDevLight:L!ghtf00t91@ds041678.mlab.com:41678/devices', (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('devices')
+
+ /*
+  let col = db.collection('device-info');
+  col.insertMany(devices, function(err, r) {
+    console.log('info posted')
+  })
+*/
+
+})
+
+app.get('/', (req, res) => {
+    let cursor =  db.collection('device-info')
+    cursor.find().toArray( (err, data) => { 
+    res.json(data)
+
+    })
+
+    //console.log(JSON.stringify(cursor))
+})
+
+
+
+app.listen(3000, () => {
+   console.log('listening on 3000')
+})
+
